71a403d839b1b7ea134dc3f04fd10efc08a05f9e
Andreas Longo <git@andreaslongo.com>
2024-06-18 22:35:26 +0200

    Add ticket new command

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,69 @@
 use clap::{Args, Parser, Subcommand};
 
 fn main() {
-    let _args = Cli::parse();
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Ticket(ticket) => match ticket.command {
+            TicketCommands::New {
+                server,
+                user,
+                password,
+                title,
+                description,
+                customer_id,
+                address_id,
+            } => {
+                println!("Dry run, this would be done:");
+                println!("  Create new ticket:");
+                println!("    Server: {server}");
+                println!("    User: {user}");
+                println!("    Ticket: {title}");
+                println!("    Description: {description}");
+                println!("    Customer ID: {customer_id}");
+                println!("    Address ID: {address_id}");
+            }
+        },
+    }
 }
-
-
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
     /// Don't do anything, just show what would be done
-    #[arg(short='n', long, global=true)]
+    #[arg(short = 'n', long, global = true)]
     dry_run: bool,
     #[command(subcommand)]
     command: Commands,
 }
 
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command(arg_required_else_help = true)]
+    Ticket(TicketArgs),
+}
+
 #[derive(Debug, Args)]
-struct VisomaLogin {
-    /// Visoma server
-    #[arg(short, long)]
-    server: String,
-    /// Visoma user
-    #[arg(short, long)]
-    user: String,
-    /// Visoma password
-    #[arg(short, long)]
-    password: String,
+#[command(args_conflicts_with_subcommands = true)]
+//#[command(flatten_help = true)]
+struct TicketArgs {
+    #[command(subcommand)]
+    command: TicketCommands,
 }
 
 #[derive(Debug, Subcommand)]
-enum Commands {
+enum TicketCommands {
     /// Creates a new ticket
-    #[command(arg_required_else_help = true)]
-    Ticket {
-        #[command(flatten)]
-        visoma_login: VisomaLogin,
+    New {
+        /// Visoma server
+        #[arg(short, long)]
+        server: String,
+        /// Visoma user
+        #[arg(short, long)]
+        user: String,
+        /// Visoma password
+        #[arg(short, long)]
+        password: String,
         /// Ticket title
         #[arg(long)]
         title: String,
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -12,7 +12,7 @@ use predicates::prelude::*;
 use std::process::Command;
 
 #[test]
-fn print_help() -> Result<(), Box<dyn std::error::Error>> {
+fn help() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("visoma-cli")?;
     cmd.arg("--help");
     cmd.assert()
@@ -25,12 +25,61 @@ fn print_help() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
-fn print_version() -> Result<(), Box<dyn std::error::Error>> {
+fn version() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("visoma-cli")?;
     cmd.arg("--version");
     cmd.assert()
         .success()
         .code(0)
         .stdout(predicate::str::is_match(r"^visoma-cli \d+\.\d+\.\d+\n$")?);
+    Ok(())
+}
+
+#[test]
+fn ticket_new_syntax() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("visoma-cli")?;
+    cmd.arg("ticket").arg("new").arg("--help");
+    cmd.assert()
+        .success()
+        .code(0)
+        .stdout(predicate::str::contains("Usage: visoma-cli ticket new [OPTIONS] --server <SERVER> --user <USER> --password <PASSWORD> --title <TITLE> --description <DESCRIPTION> --customer-id <CUSTOMER_ID> --address-id <ADDRESS_ID>"));
+    Ok(())
+}
+
+#[test]
+fn ticket_new() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("visoma-cli")?;
+    cmd.args(&[
+        "ticket",
+        "new",
+        "--dry-run",
+        "--server",
+        "example.com",
+        "--user",
+        "test",
+        "--password",
+        "test123",
+        "--title",
+        "Test Ticket",
+        "--description",
+        "A new ticket for testing",
+        "--customer-id",
+        "1",
+        "--address-id",
+        "2",
+    ]);
+    cmd.assert()
+        .success()
+        .code(0)
+        .stdout(predicate::str::contains("Dry run"))
+        .stdout(predicate::str::contains("Create new ticket"))
+        .stdout(predicate::str::contains("Server: example.com"))
+        .stdout(predicate::str::contains("User: test"))
+        .stdout(predicate::str::contains("Ticket: Test Ticket"))
+        .stdout(predicate::str::contains(
+            "Description: A new ticket for testing",
+        ))
+        .stdout(predicate::str::contains("Customer ID: 1"))
+        .stdout(predicate::str::contains("Address ID: 2"));
     Ok(())
 }
